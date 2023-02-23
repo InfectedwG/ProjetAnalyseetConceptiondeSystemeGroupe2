48a939641708f42909d1493182eb68932c2b0335
InfectedwG <maxim.1999@hotmail.com>
2023-02-22 19:31:10 -0500

    ajout des classes employe et disponibilite

---
+++ Analyse/Employe.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Analyse
+{
+    internal class Employe
+    {
+        private string nom;
+        private List<Disponibilite> disponibilites = new List<Disponibilite>();
+
+        public string Nom
+        {
+            get { return nom; }
+            set { nom = value; }
+        }
+
+        public List<Disponibilite> Disponibilites
+        {
+            get { return disponibilites; }
+        }
+
+        public void AjouterDispo(Disponibilite dispo)
+        {
+            disponibilites.Add(dispo);
+        }
+        public Disponibilite RetournerDispos(Disponibilite dispo)
+        {
+            return dispo;
+        }
+
+        public void RechercherDispo(string jour, TimeSpan duree)
+        {
+            foreach(Disponibilite dispo in Disponibilites)
+            {
+                if (dispo.ComparerDispo(jour, duree))
+                {
+                    RetournerDispos(dispo);
+                }
+            }            
+        }
+    }
+}
